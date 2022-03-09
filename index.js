25a92207d922dcaf92435e869955224972ffbf90
Willian Henrique <99339326+willianifms@users.noreply.github.com>
2022-03-09 18:56:37 +0000

    adicionei sla

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 class Retangulo{
-  constructor(largura, altura){
+  constructor(largura, altura, sla){
     this.altura = altura;
     this.largura = largura;
+    this.sla = sla;
   }
   calcularArea(){
-    return this.altura * this.largura
+    return this.altura * this.largura + this.sla
   }
   area(){
   return this.calcularArea()
 }
 
 }
-let quadrado = new Retangulo("10", "10");
-let retangulo = new Retangulo("30", "60");
+let quadrado = new Retangulo("10", "10" , "1");
+let retangulo = new Retangulo("30", "60" ,"1");
 console.log(quadrado.area())
 console.log(retangulo.area())
 
